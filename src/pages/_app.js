bd2fdc3a4a8aba920793fa6af4e5e4d08f502f32
rathod-ramraj <diamondstar2200@gmail.com>
2026-07-18 10:06:46 +0530

    Fix favicon and add to _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,11 +4,19 @@ import { useState } from 'react';
 import SplashScreen from '@/components/SplashScreen';
 import { AdProvider } from '@/contexts/AdContext';
 
+import Head from 'next/head';
+
 export default function App({ Component, pageProps, router }) {
   const [splashFinished, setSplashFinished] = useState(false);
 
   return (
     <AdProvider>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+      </Head>
       {!splashFinished && <SplashScreen onComplete={() => setSplashFinished(true)} />}
       <Layout>
         <Component {...pageProps} key={router.route} />
